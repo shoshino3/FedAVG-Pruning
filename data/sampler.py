e655aa7a38183e25a9d087e6b2aa261b22757630
Sugra <kajimusugurahoshino@gmail.com>
2024-04-23 00:59:29 -0700

    non_iid sampling

--- data/sampler.py
+++ data/sampler.py
@@ -40,6 +40,7 @@ class FederatedSampler(Sampler):
 
         return dict_users
 
+    """
     def _sample_non_iid(self) -> Dict[int, List[int]]:
         num_imgs = len(self.dataset) // self.n_shards  # 300
 
@@ -64,6 +65,38 @@ class FederatedSampler(Sampler):
                 )
 
         return dict_users
+    
+    """
+    def _sample_non_iid(self) -> Dict[int, List[int]]:
+        print("sampling non_iid")
+        min_size = 0
+        K = 10
+        N = len(self.dataset.targets)
+        dict_users = {}
+        
+        while(min_size < 10):
+            idx_batch = [[] for _ in range(self.n_clients) ]
+            
+            for k in range(K):
+                #print(np.asarray(self.dataset.targets) == k)
+                idx_k = np.where(np.asarray(self.dataset.targets) == k)[0]
+
+                np.random.shuffle(idx_k)
+                proportions = np.random.dirichlet(np.repeat(0.5, self.n_clients))
+                proportions = np.array([p*(len(idx_j)<N/self.n_clients) for p,idx_j in zip(proportions,idx_batch)])
+                proportions = proportions/proportions.sum()
+                proportions = (np.cumsum(proportions)*len(idx_k)).astype(int)[:-1]
+                idx_batch = [idx_j + idx.tolist() for idx_j,idx in zip(idx_batch,np.split(idx_k,proportions))]
+                min_size = min([len(idx_j) for idx_j in idx_batch])
+            #print("min_size:", min_size)
+        for j in range(self.n_clients):
+            print(f"Client {j}: len={len(idx_batch[j])}")
+            np.random.shuffle(idx_batch[j])
+            dict_users[j] = idx_batch[j]
+                
+
+
+        return dict_users 
 
     def set_client(self, client_id: int):
         self.client_id = client_id
